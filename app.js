59f18f5d1b91d3bdc49a2e5096f88c4dce20b1ca
Manavpreet Singh <manavpreetsingh@pop-os.localdomain>
2021-04-06 13:14:08 +0530

    added files for heruko

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin:test@cluster0.2pupg.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name : String
